73ea19c5901593481bbe5682ed79baa91914564b
Phai <dhammasornl@gmail.com>
2025-10-05 20:38:01 +0700

    update

---
+++ include/orbit.peripheral_def.h
@@ -0,0 +1,71 @@
+#ifndef NOVA_PERIPHERAL_DEF_KENDO_H
+#define NOVA_PERIPHERAL_DEF_KENDO_H
+
+#define SPI_SPEED_SD_MHZ (20)
+
+#define DELAY(MS) vTaskDelay(pdMS_TO_TICKS(MS))
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+// nova::config 
+
+constexpr auto HZ_TO_INTERVAL_MS    = [](const double FREQUENCY_HZ) -> uint32_t {
+    return static_cast<uint32_t>(1000. / FREQUENCY_HZ);
+};
+#define INTERVAL_MS_TO_HZ [](const uint32_t INTERVAL) -> uint32_t { return 1000 / INTERVAL; };
+
+#define INA236_ADDRESS = 0x40;
+
+// #define  RANGE_80MV = 0,
+// #define  RANGE_20MV = 1
+
+constexpr double TIME_TO_APOGEE_MIN  = 14.69 * 1000;
+constexpr double TIME_TO_APOGEE_MAX  = 15.69 * 1000; // Sim 14.69
+constexpr double TIME_TO_BURNOUT_MIN = 1.85  * 1000;
+constexpr double TIME_TO_BURNOUT_MAX = 2.85  * 1000; // Sim 1.85
+
+// algo
+constexpr double LAUNCH_ACC    = 30.0;   // m/s^2
+constexpr double APOGEE_VEL    = 5.0;    // m/s
+constexpr float  MAIN_ALTITUDE = 150.0f; // m
+
+#define RFD900X_BAUD         = 460800;
+#define RPI_BAUD             = 115200;
+#define UBLOX_CUSTOM_MAX_WAIT     = 250ul;  // u-blox GPS comm timeout
+#define SD_SPI_CLOCK_MHZ          = 20ul;   // 20 MHz
+#define MESSAGE_BUFFER_SIZE         = 512ul;
+
+#define TX_IDLE_INTERVAL          = HZ_TO_INTERVAL_MS(1);  // 1 Hz
+#define TX_ARMED_INTERVAL         = HZ_TO_INTERVAL_MS(2);  // 2 Hz
+#define TX_PAD_PREOP_INTERVAL     = HZ_TO_INTERVAL_MS(4);  // 4 Hz
+#define TX_ASCEND_INTERVAL        = HZ_TO_INTERVAL_MS(5);  // 5 Hz
+#define TX_DESCEND_INTERVAL       = HZ_TO_INTERVAL_MS(4);  // 4 Hz
+
+constexpr uint32_t LOG_GROUND_INTERVAL         = HZ_TO_INTERVAL_MS(1);   // 1 Hz
+constexpr uint32_t LOG_PAD_PREOP_INTERVAL    = HZ_TO_INTERVAL_MS(10);  // 10 Hz
+constexpr uint32_t LOG_APOGEE_INTERVAL       = HZ_TO_INTERVAL_MS(20);  // 20 Hz
+constexpr uint32_t LOG_LANDED_INTERVAL      = HZ_TO_INTERVAL_MS(10);  // 10 Hz
+
+// #define BUZZER_ON_INTERVAL        = 50ul;                    // 50 ms
+// #define BUZZER_IDLE_INTERVAL      = HZ_TO_INTERVAL_MS(1);    // 1 Hz
+// #define BUZZER_ARMED_INTERVAL     = HZ_TO_INTERVAL_MS(2);    // 2 Hz
+// #define BUZZER_PAD_PREOP_INTERVAL = HZ_TO_INTERVAL_MS(10);   // 10 Hz
+// #define BUZZER_ASCEND_INTERVAL    = HZ_TO_INTERVAL_MS(0.2);  // 0.2 Hz
+// #define BUZZER_DESCEND_INTERVAL   = HZ_TO_INTERVAL_MS(1);    // 1 Hz
+
+
+#define BUZZER_OFF_INTERVAL [](const uint32_t BUZZER_TOTAL_INTERVAL) -> uint32_t { return BUZZER_TOTAL_INTERVAL - BUZZER_ON_INTERVAL; };
+
+// details::assertions 
+// #define static_assert(TIME_TO_APOGEE_MAX >= TIME_TO_APOGEE_MIN, "Time to apogee is configured incorrectly!");
+// #define static_assert(TIME_TO_BURNOUT_MAX >= TIME_TO_BURNOUT_MIN, "Time to burnout is configured incorrectly!");
+
+
+// enum RadioMode {
+//   RADIO_MODE_IDLE,
+//   RADIO_MODE_TX,
+//   RADIO_MODE_RX
+// };
+// volatile RadioMode currentMode = RADIO_MODE_IDLE;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,59 @@
 #include <Arduino.h>
+#include "File_Utility.h"
+
 #include <Wire.h>
-
-// ----- LoRa -----
 #include <SPI.h>
-#include "RadioLib.h"
-#define NSS_PIN PB12
-#define NRST_PIN PB10
-#define DIO1_PIN PB9
-#define BUSY_PIN PB8
 
-SPISettings spiSettings = SPISettings(4'000'000, MSBFIRST, SPI_MODE0);
-SPIClass spi1(PB5, PB4, PA5);
-SX1262 radio = new Module(NSS_PIN, DIO1_PIN, NRST_PIN, BUSY_PIN, spi1, spiSettings);
-int transmissionState = RADIOLIB_ERR_NONE;
-volatile bool transmittedFlag = false;
-
-// ----- BME280 -----
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme;
-TwoWire myWire(PB7, PB6);   // SDA = PB7, SCL = PB6
-
-// ----- GPS -----
 #include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-TinyGPSPlus gps;
-struct GPSData {
-  double latitude = 0.0;
-  double longitude = 0.0;
-  float altitude = 0.0;
-  uint32_t timestamp = 0;
-  bool hasFix = false;
-} gpsData;
-HardwareSerial SerialGPS(2);
+#include <Adafruit_Sensor.h>
+#include "LIS3DHTR.h"
+#include <Adafruit_BME280.h>
+// #include <Servo.h>
 
+#include "SdFat.h"
+#include "RadioLib.h"
 
-int i = 0;
+#include "orbit_peripheral_def.h"
+#include "orbit_pin_def.h"
+#include "orbit_state_def.h"
+
+// Device specific
+#define THIS_FILE_PREFIX "ORBIT_LOGGER_"
+#define THIS_FILE_EXTENSION "CSV"
+
+// i2c
+TwoWire i2c1(PIN_SDA, PIN_SCL);
+Adafruit_BME280 bme1;
+LIS3DHTR<TwoWire> imu;
+
+// UARTS
+HardwareSerial gnssSerial(PIN_RX, PIN_TX);
+TinyGPSPlus lc86;
+
+// SPI
+SPIClass spi1(PIN_SPI_MOSI1, PIN_SPI_MISO1, PIN_SPI_SCK1);
+
+// SD Card
+SdSpiConfig sd_config(PIN_NSS_SD, SHARED_SPI, SD_SCK_MHZ(2), &spi1);
+using sd_t = SdFat32;
+using file_t = File32;
+FsUtil<sd_t, file_t> sd_util;
+
+// LoRa
+
+// LoRa State
+enum class LoRaState
+{
+    IDLE = 0,
+    TRANSMITTING,
+    RECEIVING
+};
+
+int status_lora;
+volatile bool tx_flag = false;
+volatile bool rx_flag = false;
+
+SPISettings lora_spi_settings(4'000'000, MSBFIRST, SPI_MODE0);
 
 constexpr struct
 {
@@ -49,158 +66,488 @@ constexpr struct
     uint16_t preamble_length = 16;
 } params;
 
-void setFlag(void) {
-  transmittedFlag = true; // LoRa
-}
+SX1262 lora = new Module(LORA_NSS, LORA_DIO1, LORA_NRST, LORA_BUSY, spi1, lora_spi_settings);
 
-void setup() {
-  Serial.begin(115200);
-  // SerialGPS.begin(115200);
-  myWire.begin();
-  
+// DATA
+struct Data
+{
+    // 40 bits
+    uint32_t timestamp;
+    uint8_t counter;
 
-  // ----- LoRa [Config] -----
-  spi1.begin();
-  pinMode(NSS_PIN, OUTPUT);
-  digitalWrite(NSS_PIN, HIGH);
-  int state = radio.begin();
-  radio.setFrequency(915.0);
-  radio.setSpreadingFactor(12);
-  radio.setBandwidth(125.0);
-  radio.setCodingRate(8);
-  radio.setSyncWord(0x12);
-  if (state != RADIOLIB_ERR_NONE) {
-    while (true) {
-      Serial.println("LoRa init failed, error code: " + String(state));
-      delay(10);
+    String state;
+
+    // 160 bits
+    double gps_latitude;
+    double gps_longitude;
+    float gps_altitude;
+
+    // 96 bits
+    float altitude;
+    float temp;
+    float humidity;
+    float press;
+
+    // 384 bits
+    struct
+    {
+        float x = 0;
+        float y = 0;
+        float z = 0;
+    } acc,vel;
+} data;
+
+// State
+bool sdstate;
+
+struct
+{
+    float altitude_offset{};
+    float apogee;
+} ground_truth;
+
+// Communication data
+String constructed_data;
+String tx_data;
+
+// time on function
+struct plusTime{ // millis
+    uint32_t led_control = 10; // LED
+    uint32_t read_gnss = 10; // GPS
+    uint32_t read_bme = 10; // BME
+    uint32_t read_imu = 10; // IMU
+    uint32_t construct_data = 10; // construct data
+    uint32_t transmit_data = 10; // LORA
+    uint32_t send_data = 2000; 
+    uint32_t save_data = 100; // SD card
+    uint32_t log_data = LOG_GROUND_INTERVAL;
+    uint32_t save_data_to_sd_card = 1000;
+    uint32_t print_data = 2000; // Serail monitor
+    uint32_t check_state = 10; // Stage
+}plusTime;
+
+// delay time on function
+struct nowTime{
+    uint32_t led_control = millis(); // LED
+    uint32_t read_gnss = millis(); // GPS
+    uint32_t read_bme = millis(); // BME
+    uint32_t read_imu = millis(); // IMU
+    uint32_t construct_data = millis();
+    uint32_t transmit_data = millis(); // LORA
+    uint32_t send_data = millis();
+    uint32_t save_data = millis(); // SD card
+    uint32_t log_data = millis();
+    uint32_t save_data_to_sd_card = millis();
+    uint32_t print_data = millis(); // Serial monitor
+    uint32_t check_state = millis(); // Stage
+}nowTime;
+
+// variables
+volatile bool wake_flag = false;
+
+extern void led_control();
+
+extern void read_gnss();
+
+extern void read_bme();
+
+extern void read_imu();
+
+extern void calculate_vel();
+
+extern void construct_data();
+
+extern void transmit_data();
+
+extern void save_data();
+
+extern void print_data();
+
+extern void check_state();
+
+extern void set_txflag();
+
+template <typename SdType, typename FileType>
+extern void init_storage(FsUtil<SdType, FileType> &sd_util_instance);
+
+void setup()
+{
+    Serial.begin(460800);
+    delay(2000);
+
+    i2c1.begin();
+    i2c1.setClock(300000u);
+
+    spi1.begin();
+
+    // GPIO
+    pinMode(ledPin1, OUTPUT); // LED
+    pinMode(ledPin2, OUTPUT); // LED
+    pinMode(ledPin3, OUTPUT); // LED
+    pinMode(ledPin4, OUTPUT); // LED
+
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    delay(200); // 200 ms ON
+
+    // Turn all LEDs OFF
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    // variable
+    static bool state;
+
+    pinMode(LORA_NSS, OUTPUT);
+    digitalWrite(LORA_NSS, HIGH); // deselect LoRa
+
+    // pinMode(PIN_NSS_SD, OUTPUT);
+    // digitalWrite(PIN_NSS_SD, HIGH); // idle SD CS high
+
+    /* ==================================== SD card ==================================== */
+
+    sd_util.sd().begin(sd_config);
+
+    init_storage(sd_util);
+    Serial.println("SD SUCESS");
+    digitalWrite(ledPin1, 1);
+    
+
+    /* ==================================== LoRa ==================================== */
+
+    int lora_state = lora.begin(params.center_freq,
+                                     params.bandwidth,
+                                     params.spreading_factor,
+                                     params.coding_rate,
+                                     params.sync_word,
+                                     params.power,
+                                     params.preamble_length,
+                                     0,
+                                     false);
+    state = state || lora.explicitHeader();
+    state = state || lora.setCRC(true);
+    state = state || lora.autoLDRO();
+
+    lora.setPacketSentAction(set_txflag);
+
+    if (lora_state == RADIOLIB_ERR_NONE)
+    {
+        Serial.println("SX1262 initialized successfully!");
+        digitalWrite(ledPin2, 1);
     }
-  }
-  radio.setPacketSentAction(setFlag);
-  
-  // ----- BME280 [Check] -----
-  if (!bme.begin(0x76, &myWire)) {
-    while (1){
-      Serial.println("BME280 not found!");
-      delay(1000);
+    else
+    {
+        Serial.print("Initialization failed! Error: ");
+        while (true){
+            Serial.println(lora_state);
+            delay(1000);
+        }
     }
-  }
-}
 
-// int count = 0;
+    /* ==================================== LIS3DHTR (IMU) ==================================== */
 
-/*
-void read_gnss() {
-  // Feed TinyGPS++ with all available bytes from the GNSS serial
-  while (SerialGPS.available()) {
-    gps.encode((char)SerialGPS.read());
-  }
+    imu.begin(i2c1,0x18);
+    
+    imu.setFullScaleRange(LIS3DHTR_RANGE_16G); // 6, 12, or 24 G
+    imu.setOutputDataRate(LIS3DHTR_DATARATE_1_6KH);
+    
+    /* ==================================== lc86g UARTS ==================================== */
 
-  // When location data is updated, copy fields to gpsData
-  if (gps.location.isUpdated()) {
-    gpsData.latitude = gps.location.lat();
-    gpsData.longitude = gps.location.lng();
-    gpsData.hasFix = gps.location.isValid();
-  }
+    gnssSerial.begin(115200);
 
-  if (gps.altitude.isUpdated()) {
-    gpsData.altitude = static_cast<float>(gps.altitude.meters());
-  }
+    /* ==================================== bme280 ==================================== */
 
-  // GPS time may update independently
-  if (gps.time.isUpdated()) {
-    // time.value() returns hhmmss as integer (e.g., 123045)
-    gpsData.timestamp = gps.time.value();
-  }
-}
+    float gnd = 0.f;
 
-void print_gps_data() {
-  Serial.println(F("----- GPS -----"));
-  if (gpsData.hasFix) {
-    Serial.print(F("Latitude : "));
-    Serial.println(gpsData.latitude, 6);
-
-    Serial.print(F("Longitude: "));
-    Serial.println(gpsData.longitude, 6);
-
-    Serial.print(F("Altitude : "));
-    Serial.print(gpsData.altitude, 2);
-    Serial.println(F(" m"));
-
-    Serial.print(F("Timestamp: "));
-    // Print hhmmss as HH:MM:SS for readability
-    uint32_t t = gpsData.timestamp;
-    uint8_t hh = (t / 10000) % 100;
-    uint8_t mm = (t / 100) % 100;
-    uint8_t ss = t % 100;
-    if (t > 0) {
-      if (hh < 10) Serial.print('0');
-      Serial.print(hh);
-      Serial.print(':');
-      if (mm < 10) Serial.print('0');
-      Serial.print(mm);
-      Serial.print(':');
-      if (ss < 10) Serial.print('0');
-      Serial.println(ss);
-    } else {
-      Serial.println(F("N/A"));
+    if(!bme1.begin(0x76, &i2c1)){
+        Serial.println("BME NOT FOUND");
     }
-  } else {
-    Serial.println(F("No fix yet"));
-  }
-  Serial.println(F("----------------\n"));
+    else
+    {
+        bme1.SAMPLING_X16;
+        for (size_t i = 0; i < 20; ++i)
+        {
+            read_bme();
+        }
+        gnd += data.altitude;
+    }
+    ground_truth.altitude_offset = gnd;
+
+    /* ==================================== START ==================================== */
+   
+    Serial.println("START TASK");
 }
 
-unsigned long lastPrint = 0;
-*/
+void loop()
+{
+    
+    if(millis() > nowTime.read_gnss + plusTime.read_gnss){
+        read_gnss();
+        nowTime.read_gnss = millis();
+    }
 
-void loop(){
-  i += 1;
+    if(millis() > nowTime.read_bme + plusTime.read_bme){
+        read_bme();
+        nowTime.read_bme = millis(); // BME
+    }
 
-  // ----- Bme280 ------
+    if(millis() > nowTime.read_imu + plusTime.read_imu){
+        read_imu();
+        nowTime.read_imu = millis(); // IMU
+    }
 
-  Serial.print("#");
-  Serial.println(i);
+    if(millis() > nowTime.construct_data + plusTime.construct_data){
+        construct_data();
+        nowTime.construct_data = millis();
+    }
 
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" °C");
+    if(millis() > nowTime.transmit_data + plusTime.transmit_data){
+        transmit_data();
+        nowTime.transmit_data = millis(); // LORA
+    }
 
-  Serial.print("Pressure = ");
+    if(millis() > nowTime.save_data + plusTime.save_data){
+        save_data();
+        nowTime.save_data = millis(); // SD card
+    }
 
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println(" hPa");
+    if(millis() > nowTime.print_data + plusTime.print_data){
+        print_data();
+        nowTime.print_data = millis();
+    }
 
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
+    if(millis() > nowTime.check_state + plusTime.check_state){
+        check_state();
+        nowTime.check_state = millis();
+    }
+}
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
 
-  Serial.println();
+/* ======================================= LOOP ======================================= */
 
-  // read_gnss();
-  // // Print when location updated OR every 1 second as fallback
-  // if (gps.location.isUpdated() || (millis() - lastPrint > 1000)) {
-  //   print_gps_data();
-  //   lastPrint = millis();
-  // }
+void led_control()
+{
+    // digitalWrite(ledPin4, !digitalRead(ledPin4));
+}
 
-  // ----- LoRa -----
-  // if(transmittedFlag) {
-  //   transmittedFlag = false;
+void read_gnss()
+{
 
-  //   if (transmissionState == RADIOLIB_ERR_NONE) {
-  //   }
+    while (gnssSerial.available())
+    {
+        lc86.encode(gnssSerial.read());
+        data.timestamp = lc86.time.value();
+    }
 
-  //   radio.finishTransmit();
-  //   delay(1000);
+    if (lc86.location.isUpdated())
+    {
+        data.gps_latitude = lc86.location.lat();
+        data.gps_longitude = lc86.location.lng();
+        data.gps_altitude = lc86.altitude.meters();
+    }
+}
 
-  //   String str = "Hello World! #" + String(count++);
-  //   transmissionState = radio.startTransmit(str);
-  //   Serial.println("Sending: " + str);
-  // }
+void read_bme()
+{
 
-  delay(1000);
+    data.temp = bme1.readTemperature();
+    data.press = bme1.readPressure() / 100.0F;
+    data.humidity = bme1.readHumidity();
+    data.altitude = bme1.readAltitude(SEALEVELPRESSURE_HPA);
+    // bme1.readHumidity();
+}
+
+void read_imu()
+{
+    data.acc.x = imu.getAccelerationX();
+    data.acc.y = imu.getAccelerationY();
+    data.acc.z = imu.getAccelerationZ();
+    calculate_vel();
+}
+
+void calculate_vel()
+{
+    data.vel.x += data.acc.x * (plusTime.read_imu/1000.0f);
+    data.vel.y += data.acc.y * (plusTime.read_imu/1000.0f);
+    data.vel.z += data.acc.z * (plusTime.read_imu/1000.0f);
+}
+
+void construct_data()
+{
+    constructed_data = "";
+    constructed_data +=
+        String(data.counter) + ',' +
+        String(data.timestamp) + ',' +
+        data.state + ',' +
+        String(data.gps_latitude, 6) + ',' +
+        String(data.gps_longitude, 6) + ',' +
+        String(data.altitude, 4) + ',' +
+        String(ground_truth.apogee, 4) + ',' +
+        String(data.temp) + ',' +
+        String(data.press) + ',' +
+        String(data.humidity) + ',' +
+        String(data.acc.x) + ',' + 
+        String(data.acc.y) + ',' + 
+        String(data.acc.z) + '\n';
+
+    tx_data = "";
+    tx_data += 
+        "<20>" + ',' + // DEVICE NO 
+        String(params.center_freq) + ',' +
+        String(data.counter) + ',' +
+        data.state + ',' +
+        String(data.gps_latitude, 6) + ',' +
+        String(data.gps_longitude, 6) + ',' +
+        String(data.altitude, 4) + ',' +
+        String(ground_truth.apogee, 4);
+}
+
+
+template <typename SdType, typename FileType>
+void init_storage(FsUtil<SdType, FileType> &sd_util_instance)
+{
+    sd_util_instance.find_file_name(THIS_FILE_PREFIX, THIS_FILE_EXTENSION);
+    sd_util_instance.template open_one<FsMode::WRITE>();
+}
+
+void save_data()
+{
+    if(millis() > nowTime.log_data + plusTime.log_data){
+        sd_util.file() << constructed_data;
+        //  Serial.println("Data written and flushed.");
+    }
+
+    if(millis() > nowTime.save_data_to_sd_card + plusTime.save_data_to_sd_card){
+        sd_util.flush_one();
+        nowTime.save_data_to_sd_card = millis();
+    }
+}
+
+void transmit_data()
+{
+    // Tx Loop
+    if(millis() > nowTime.send_data + plusTime.send_data && tx_flag){
+        lora.startTransmit(tx_data);
+        Serial.println("[TRANSMITTING...]"); 
+        ++data.counter;
+        nowTime.send_data = millis();
+    }
+    // On Transmit
+    if (!tx_flag)
+    {
+        Serial.print("[TRANSMITTED] ");
+        Serial.println(tx_data);
+        tx_flag = true;
+    }
+}
+
+void check_state()
+{
+    const double alt_x = data.altitude - ground_truth.altitude_offset;
+    const double vel_x = data.vel.x;
+    const double acc = data.acc.x;
+
+    if(data.state == "GROUND")
+    {
+        // Next: WAIT FOR POWER
+        if(acc >= LAUNCH_ACC){
+            plusTime.log_data = LOG_PAD_PREOP_INTERVAL;
+            data.state = "RISING";
+        }
+    }
+    else if(data.state == "RISING")
+    {
+        // Next: WAIT FOR APOGEE
+        if(acc < LAUNCH_ACC){
+            plusTime.log_data = LOG_APOGEE_INTERVAL;
+            data.state = "COASTING";
+        }
+    }
+    else if(data.state == "COASTING")
+    {
+        // Next: AT APOGEE WAIT FOR LANDING
+        if(vel_x < APOGEE_VEL){
+            plusTime.log_data = LOG_LANDED_INTERVAL;
+            data.state = "LANDED";
+        }
+    }
+    else if(data.state == "LANDED")
+    {
+        // Next: WAIT FOR LANDED
+    }
+
+    if (alt_x > ground_truth.apogee)
+    {
+        ground_truth.apogee = alt_x;
+    }
+}
+
+void print_data()
+{
+    Serial.println("====== DATA ======");
+
+    Serial.print("Timestamp: ");
+    Serial.println(data.timestamp);
+
+    Serial.print("Counter: ");
+    Serial.println(data.counter);
+
+    Serial.println("---- STATES ----");
+    Serial.print("STATE: ");
+    Serial.println(data.state);
+
+    Serial.println("---- GPS ----");
+    Serial.print("Latitude: ");
+    Serial.println(data.gps_latitude, 6);
+    Serial.print("Longitude: ");
+    Serial.println(data.gps_longitude, 6);
+    Serial.print("Altitude: ");
+    Serial.println(data.gps_altitude, 4);
+
+    Serial.println("---- ENV ----");
+    Serial.print("Temperature: ");
+    Serial.println(data.temp, 2);
+    Serial.print("Altitude: ");
+    Serial.println(data.altitude, 2);
+    Serial.print("Pressure: ");
+    Serial.println(data.press, 2);
+    Serial.print("Humidity: ");
+    Serial.println(data.humidity, 2);
+
+    Serial.println("---- IMU ACC ----");
+    Serial.print("X: ");
+    Serial.print(data.acc.x, 3);
+    Serial.print("  Y: ");
+    Serial.print(data.acc.y, 3);
+    Serial.print("  Z: ");
+    Serial.println(data.acc.z, 3);
+
+    Serial.println("---- VEL ----");
+    Serial.print("X: ");
+    Serial.print(data.vel.x, 3);
+    Serial.print("  Y: ");
+    Serial.print(data.vel.y, 3);
+    Serial.print("  Z: ");
+    Serial.println(data.vel.z, 3);
+
+
+    // Serial to ground web
+    // Serial.print(String(data.acc.x, 3) + ',' 
+    //             + String(data.acc.y, 3) + ',' 
+    //             + String(data.acc.z, 3) + ',' 
+    //             + String(data.gps_latitude, 6) + ',' 
+    //             + String(data.gps_longitude, 6) + ',' 
+    //             + String(data.temp, 2) + ',' 
+    //             + String(data.press, 2) + ','
+    //             + String(data.humidity, 2) + ','
+    //             + String("100") + ','
+    //             + String("RISING")
+    //         );
+}
+
+void set_txflag()
+{
+    tx_flag = false;
 }
